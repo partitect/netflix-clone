2a3a2d9789ea7eee815c7819f883e09aeb4f0230
Telat Kaya <telatkaya@yandex.com>
2021-03-30 01:44:58 +0300

    add bottomnavigation

--- lib/screens/stretch_head.dart
+++ lib/screens/stretch_head.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:netflix_clone/data/top_rated_movie_json.dart';
 import 'package:netflix_clone/data/trending_movie_json.dart';
 import 'package:netflix_clone/data/trending_tv_json.dart';
+import 'package:netflix_clone/data/upcoming_movie_json.dart';
 import 'package:sliver_stretch_header/sliver_stretch_header.dart';
 
 class StretchHeader extends StatefulWidget {
@@ -11,16 +12,23 @@ class StretchHeader extends StatefulWidget {
 }
 
 class _StretchHeaderState extends State<StretchHeader> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: HexColor("171717"),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
             brightness: Brightness.dark,
             pinned: true,
-            backgroundColor: Colors.black.withOpacity(.7),
+            backgroundColor: Colors.black.withOpacity(.1),
             title: Container(
               padding: EdgeInsets.symmetric(horizontal: 50),
               child: Row(
@@ -62,9 +70,9 @@ class _StretchHeaderState extends State<StretchHeader> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    margin: EdgeInsets.only(bottom: 10),
+                    margin: EdgeInsets.only(bottom: 10, top: 5),
                     child: Text(
-                      "Popular Tv Shows on Netflix",
+                      "Popular Tv Shows",
                       style: GoogleFonts.quicksand(
                         textStyle: TextStyle(
                             color: Colors.white,
@@ -74,51 +82,31 @@ class _StretchHeaderState extends State<StretchHeader> {
                     ),
                   ),
                   Container(
-                    height: 200.0,
+                    height: 180.0,
                     //margin: EdgeInsets.all(5),
                     child: ListView.builder(
                       cacheExtent: 1000.0,
                       scrollDirection: Axis.horizontal,
                       itemCount: trendingTvList[0]["results"].length,
                       itemBuilder: (context, index) {
-                        var posterPath = trendingTvList[0]["results"][index]
-                            ["backdrop_path"];
+                        var posterPath =
+                            trendingTvList[0]["results"][index]["poster_path"];
                         return Container(
                           margin: EdgeInsets.only(right: 7),
                           padding: EdgeInsets.all(10),
                           decoration: BoxDecoration(
                             color: Colors.brown,
                             borderRadius: BorderRadius.all(
-                              Radius.circular(2),
+                              Radius.circular(5),
                             ),
                             image: DecorationImage(
                               image: NetworkImage(
                                   "https://image.tmdb.org/t/p/original$posterPath"),
                               fit: BoxFit.cover,
-                              colorFilter: ColorFilter.mode(
-                                Colors.red.withOpacity(.5),
-                                BlendMode.darken,
-                              ),
                             ),
                           ),
                           width: MediaQuery.of(context).size.width / 3,
-                          height: 200,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                trendingTvList[0]["results"][index]["name"] ??
-                                    "No Name",
-                                style: GoogleFonts.quicksand(
-                                  textStyle: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w900),
-                                ),
-                              ),
-                            ],
-                          ),
+                          height: 180,
                         );
                       },
                     ),
@@ -135,9 +123,9 @@ class _StretchHeaderState extends State<StretchHeader> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    margin: EdgeInsets.only(bottom: 10),
+                    margin: EdgeInsets.only(bottom: 10, top: 5),
                     child: Text(
-                      "Popular Movie on Netflix",
+                      "Popular Movies",
                       style: GoogleFonts.quicksand(
                         textStyle: TextStyle(
                             color: Colors.white,
@@ -147,7 +135,7 @@ class _StretchHeaderState extends State<StretchHeader> {
                     ),
                   ),
                   Container(
-                    height: 200.0,
+                    height: 180.0,
                     //margin: EdgeInsets.all(5),
                     child: ListView.builder(
                       cacheExtent: 1000.0,
@@ -155,44 +143,23 @@ class _StretchHeaderState extends State<StretchHeader> {
                       itemCount: trendingMovieList[0]["results"].length,
                       itemBuilder: (context, index) {
                         var posterPath = trendingMovieList[0]["results"][index]
-                            ["backdrop_path"];
+                            ["poster_path"];
                         return Container(
                           margin: EdgeInsets.only(right: 7),
                           padding: EdgeInsets.all(10),
                           decoration: BoxDecoration(
                             color: Colors.brown,
                             borderRadius: BorderRadius.all(
-                              Radius.circular(2),
+                              Radius.circular(5),
                             ),
                             image: DecorationImage(
                               image: NetworkImage(
                                   "https://image.tmdb.org/t/p/original$posterPath"),
                               fit: BoxFit.cover,
-                              colorFilter: ColorFilter.mode(
-                                Colors.red.withOpacity(.5),
-                                BlendMode.darken,
-                              ),
                             ),
                           ),
                           width: MediaQuery.of(context).size.width / 3,
-                          height: 200,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                trendingMovieList[0]["results"][index]
-                                        ["title"] ??
-                                    "No Name",
-                                style: GoogleFonts.quicksand(
-                                  textStyle: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w900),
-                                ),
-                              ),
-                            ],
-                          ),
+                          height: 180,
                         );
                       },
                     ),
@@ -209,9 +176,9 @@ class _StretchHeaderState extends State<StretchHeader> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    margin: EdgeInsets.only(bottom: 10),
+                    margin: EdgeInsets.only(bottom: 10, top: 5),
                     child: Text(
-                      "Top Rated Films on Netflix",
+                      "Top Rated Movies",
                       style: GoogleFonts.quicksand(
                         textStyle: TextStyle(
                             color: Colors.white,
@@ -221,7 +188,7 @@ class _StretchHeaderState extends State<StretchHeader> {
                     ),
                   ),
                   Container(
-                    height: 200.0,
+                    height: 180.0,
                     //margin: EdgeInsets.all(5),
                     child: ListView.builder(
                       cacheExtent: 1000.0,
@@ -229,44 +196,76 @@ class _StretchHeaderState extends State<StretchHeader> {
                       itemCount: topRatedMovieList[0]["results"].length,
                       itemBuilder: (context, index) {
                         var posterPath = topRatedMovieList[0]["results"][index]
-                            ["backdrop_path"];
+                            ["poster_path"];
                         return Container(
                           margin: EdgeInsets.only(right: 7),
                           padding: EdgeInsets.all(10),
                           decoration: BoxDecoration(
                             color: Colors.brown,
                             borderRadius: BorderRadius.all(
-                              Radius.circular(2),
+                              Radius.circular(5),
                             ),
                             image: DecorationImage(
                               image: NetworkImage(
                                   "https://image.tmdb.org/t/p/original$posterPath"),
                               fit: BoxFit.cover,
-                              colorFilter: ColorFilter.mode(
-                                Colors.red.withOpacity(.5),
-                                BlendMode.darken,
-                              ),
                             ),
                           ),
                           width: MediaQuery.of(context).size.width / 3,
-                          height: 200,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                topRatedMovieList[0]["results"][index]
-                                        ["title"] ??
-                                    "No Name",
-                                style: GoogleFonts.quicksand(
-                                  textStyle: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w900),
-                                ),
-                              ),
-                            ],
+                          height: 180,
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              padding: EdgeInsets.all(5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10, top: 5),
+                    child: Text(
+                      "Upcoming Movies",
+                      style: GoogleFonts.quicksand(
+                        textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 180.0,
+                    //margin: EdgeInsets.all(5),
+                    child: ListView.builder(
+                      cacheExtent: 1000.0,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: upcomingMovieList[0]["results"].length,
+                      itemBuilder: (context, index) {
+                        var posterPath = upcomingMovieList[0]["results"][index]
+                            ["poster_path"];
+                        return Container(
+                          margin: EdgeInsets.only(right: 7),
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.brown,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5),
+                            ),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://image.tmdb.org/t/p/original$posterPath"),
+                              fit: BoxFit.cover,
+                            ),
                           ),
+                          width: MediaQuery.of(context).size.width / 3,
+                          height: 180,
                         );
                       },
                     ),
@@ -277,6 +276,52 @@ class _StretchHeaderState extends State<StretchHeader> {
           ),
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        selectedFontSize: 14,
+        unselectedFontSize: 14,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.grey,
+            ),
+            label: 'Ana Sayfa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.video_collection_outlined,
+              color: Colors.grey,
+            ),
+            label: 'Çok Yakında',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.download_outlined,
+              color: Colors.grey,
+            ),
+            label: 'İndirilenler',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        unselectedLabelStyle: TextStyle(color: Colors.red),
+        onTap: _onItemTapped,
+      ),
     );
   }
+}
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 }
